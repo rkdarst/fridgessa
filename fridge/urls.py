98ea0ca5c6ad3b04d049ed9a2006c2720e9d09bf
Richard Darst <rkd@zgib.net>
2014-09-18 11:44:47 +0300

    Minimally working inventory view

---
+++ fridge/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'fridgessa.views.home', name='home'),
+    # url(r'^fridgessa/', include('fridgessa.foo.urls')),
+
+    url(r'^inventory/$', views.inventory),
+)
--- fridge/views.py
+++ fridge/views.py
@@ -1,1 +1,21 @@
 # Create your views here.
+
+from functools import partial
+import json
+import logging
+import os.path
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django import forms
+from django.contrib import messages
+
+from . import models
+from .models import Location, Item, Product, Shelf
+
+
+def inventory(request):
+    items = Item.objects.all()
+    return render(request, 'fridge/inventory.html', locals())
+
